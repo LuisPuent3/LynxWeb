55b6b339bfa6866650d97b5ae15b5548796f411f
LuisPuent3 <92196989+LuisPuent3@users.noreply.github.com>
2024-12-14 21:44:54 -0600

    post login

--- backed/index.js
+++ backed/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/productos', productRoutes);
 app.use('/api/pedidos', orderRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
